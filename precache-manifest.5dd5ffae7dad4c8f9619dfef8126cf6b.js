a6286e6d4b88c6157df1790b6431351c41a49607
Nikola Bojanovic <nikolabojanovicmob@gmail.com>
2019-04-01 19:58:29 +0200

    Updates

---
+++ precache-manifest.5dd5ffae7dad4c8f9619dfef8126cf6b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "dad51b5cb92572d43e00",
+    "url": "/react-table/static/js/runtime~main.15ef8938.js"
+  },
+  {
+    "revision": "c0331429d30490dfa0a4",
+    "url": "/react-table/static/js/main.25f065d7.chunk.js"
+  },
+  {
+    "revision": "1ed868cf8bbe0f8913c1",
+    "url": "/react-table/static/js/2.6f40156a.chunk.js"
+  },
+  {
+    "revision": "dc744e3aab239b1f4043038b07975aef",
+    "url": "/react-table/index.html"
+  }
+];
